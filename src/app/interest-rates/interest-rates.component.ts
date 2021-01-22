307ca5d5a4ac915dc87e6a1929d58f083fe6b832
hkstwk <hkolvoort+github@gmail.com>
2021-01-22 23:33:00 +0100

    Working filter (finally!)

--- src/app/interest-rates/interest-rates.component.ts
+++ src/app/interest-rates/interest-rates.component.ts
@@ -2,14 +2,24 @@ import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
 import { InterestService } from "../interest.service";
 
+interface InterestData {
+  id: number;
+  Hypotheek: string;
+  NHG: number;
+  "60%": number;
+  "80%": number;
+  "100%": number;
+}
+
 @Component({
   selector: "app-interest-rates",
   templateUrl: "./interest-rates.component.html",
   styleUrls: ["./interest-rates.component.css"]
 })
 export class InterestRatesComponent implements OnInit {
-  interestRateList;
-  searchValue: string = '';
+  interestRateList: InterestData[] = [];
+  private interestRateListUnFiltered: InterestData[] = [];
+  searchValue: string = "";
 
   constructor(
     private http: HttpClient,
@@ -21,21 +31,33 @@ export class InterestRatesComponent implements OnInit {
   }
 
   getInterestRates(period: string) {
-    this.interestService.getInterestRates(period).subscribe(interestRates => {
-      this.interestRateList = interestRates;
-    });
+    this.interestService
+      .getInterestRates(period)
+      .subscribe((interestRates: InterestData[]) => {
+        for (let rate of interestRates) {
+          this.interestRateList.push(rate);
+        }
+        // Copy interestRate[] to unfiltered []
+        this.interestRateListUnFiltered = this.interestRateList.slice();
+      });
   }
 
   changePeriod(event) {
+    this.interestRateList.splice(0);
     this.getInterestRates(event.target.value);
-    this
   }
 
-  onSearchChange(event){
-    this.searchValue =  event.target.value;
-    console.log(this.searchValue);
-    this.interestRateList.filter(item =>​
-      item.Hypotheek.toLowerCase().includes(this.searchValue.toLowerCase())​
-    );​
+  onSearchChange(event) {
+    // restore unfiltered []
+    this.interestRateList = this.interestRateListUnFiltered.slice();
+
+    // apply (new) filter
+    this.interestRateList = this.interestRateList.filter(
+      (interestData: InterestData) => {
+        return interestData.Hypotheek.toLowerCase().includes(
+          event.target.value.toLowerCase()
+        );
+      }
+    );
   }
 }
